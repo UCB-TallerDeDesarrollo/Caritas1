6c328bd870b83971a493b7763b7016ee00642b32
alejandroMaldonado <alejandro.amg@hotmail.com>
2011-10-10 23:29:49 -0430

    mas validaciones

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -9,12 +9,14 @@ class Volunteer < ActiveRecord::Base
   validates_presence_of :position
   validates_presence_of :profession
   validates_presence_of :date_of_birth
-  validates_presence_of :genre    
+  validates_presence_of :genre      
   validates_numericality_of :phone_number, :greater_than => 0, :if => "self.phone_number.present?"
-  validates_numericality_of :movil, :greater_than => 0, :if => "self.movil.present?"
+  validates_numericality_of :movil, :greater_than => 0, :if => "self.movil.present?"  
   validates_length_of :profession, :within => 0..50
-  validates_length_of :position, :within => 0..50
+  validates_length_of :position, :within => 0..20
   validates_length_of :name, :within => 0..50
   validates_length_of :last_name, :within => 0..50
-  validates_length_of :movil, :within => 0..50  
+  validates_length_of :second_last_name, :within => 0..50
+  validates_length_of :movil, :within => 8..8  
+  validates_length_of :phone_number, :within => 7..7
 end
