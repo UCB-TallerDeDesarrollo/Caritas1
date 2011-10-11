97da3380bd280742463248e11b2cdab343f07b7f
alejandroMaldonado <alejandro.amg@hotmail.com>
2011-10-10 22:31:19 -0430

    validaciones voluntarios

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,2 +1,19 @@
 class Volunteer < ActiveRecord::Base
+  
+  #put validates here
+  validates_presence_of :name
+  validates_presence_of :last_name
+  validates_presence_of :second_last_name
+  validates_presence_of :phone_number
+  validates_presence_of :movil
+  validates_presence_of :position
+  validates_presence_of :date_of_birth
+  validates_presence_of :genre    
+  validates_numericality_of :phone_number, :greater_than => 0, :if => "self.phone_number.present?"
+  validates_numericality_of :movil, :greater_than => 0, :if => "self.movil.present?"
+  validates_length_of :profession, :within => 0..50
+  validates_length_of :position, :within => 0..50
+  validates_length_of :name, :within => 0..50
+  validates_length_of :last_name, :within => 0..50
+  validates_length_of :movil, :within => 0..50  
 end
