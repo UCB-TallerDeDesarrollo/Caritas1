1f05fdf614e442e78313dac0bd4afc72c85e6f41
Valeria Tejada <vatesa09@gmail.com>
2011-10-12 08:37:21 -0400

    platilla para mantener los modelos ordenados

--- app/models/campaing.rb
+++ app/models/campaing.rb
@@ -1,16 +1,22 @@
 class Campaing < ActiveRecord::Base
-  
- 
-    
+  #put constants here
+
+  #put relations and references here
+
+  #put active record callbacks here
+
+  #put validates here
   validates_presence_of :name
   validates_presence_of :location
  
   validates_presence_of :responsible
   validates_presence_of :state
   
-  
   validates_length_of :name, :within => 0..100
   validates_length_of :location, :within => 0..80
   validates_format_of :name, :with => /^([a-zA-Z\ \-]{3,100})$/i
-  
+
+  #put class methods here
+
+  #put object methods here
 end
--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,13 +1,9 @@
 class Volunteer < ActiveRecord::Base
+  #put constants here
 
-def self.search(search)
-  if search
-    find(:all, :conditions => ['name LIKE ? OR last_name LIKE ? OR second_last_name LIKE ? OR profession LIKE ?', "%#{search}%","%#{search}%","%#{search}%","%#{search}%"])
-  else
-    find(:all)
-  end
-end
+  #put relations and references here
 
+  #put active record callbacks here
 
   #put validates here
   validates_presence_of :name
@@ -23,10 +19,22 @@ end
   validates_numericality_of :movil, :greater_than => 0, :if => "self.movil.present?"
   validates_length_of :profession, :within => 0..50
   validates_length_of :position, :within => 0..20
-  validates_inclusion_of :movil, :in => 10000000..99999999, :message => "fuera de rango"
-  validates_inclusion_of :phone_number, :in => 1000000..9999999, :message => "fuera de rango"
+  validates_inclusion_of :movil, :in => 10000000..99999999, :message => "Fuera de rango"
+  validates_inclusion_of :phone_number, :in => 1000000..9999999, :message => "Fuera de rango"
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{,50})\Z/
   validates_format_of :name, :with => /^([a-zA-Z\ ]{3,50})$/i
   validates_format_of :last_name, :with => /^([a-zA-Z\ \-]{3,50})$/i
   validates_format_of :second_last_name, :with => /^([a-zA-Z\ \-]{3,50})$/i
+
+  #put class methods here
+  def self.search(search)
+    if search
+      find(:all, :conditions => ['name LIKE ? OR last_name LIKE ? OR second_last_name LIKE ? OR profession LIKE ?', "%#{search}%","%#{search}%","%#{search}%","%#{search}%"])
+    else
+      find(:all)
+    end
+  end
+
+  #put object methods here
+
 end
