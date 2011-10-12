76003ab9846cacd07f4832b487d4aa9b6454b062
Javier Pardo Gomez <javier_p_g1989@hotmail.com>
2011-10-11 21:34:46 -0400

    Se agrego el filtro de busqueda en voluntarios

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,5 +1,14 @@
 class Volunteer < ActiveRecord::Base
-  
+
+def self.search(search)
+  if search
+    find(:all, :conditions => ['name LIKE ? OR last_name LIKE ? OR second_last_name LIKE ? OR profession LIKE ?', "%#{search}%","%#{search}%","%#{search}%","%#{search}%"])
+  else
+    find(:all)
+  end
+end
+
+
   #put validates here
   validates_presence_of :name
   validates_presence_of :last_name
@@ -10,13 +19,13 @@ class Volunteer < ActiveRecord::Base
   validates_presence_of :profession
   validates_presence_of :date_of_birth
   validates_presence_of :genre      
-  validates_numericality_of :phone_number, :greater_than => 0, :if => "self.phone_number.present?"
-  validates_numericality_of :movil, :greater_than => 0, :if => "self.movil.present?"  
+  #validates_numericality_of :phone_number, :greater_than => 0, :if => "self.phone_number.present?"
+  #validates_numericality_of :movil, :greater_than => 0, :if => "self.movil.present?"
   validates_length_of :profession, :within => 0..50
   validates_length_of :position, :within => 0..20
   validates_length_of :name, :within => 0..50
   validates_length_of :last_name, :within => 0..50
   validates_length_of :second_last_name, :within => 0..50
-  validates_length_of :movil, :within => 8..8  
-  validates_length_of :phone_number, :within => 7..7
+  #validates_length_of :movil, :within => 8..8
+  # validates_length_of :phone_number, :within => 7..7
 end
