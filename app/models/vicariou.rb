a9cd25246b69b2871af5ec3439e3ebe65383fe2f
LeonJorge <jorgerodrigoleonrgit@gmail.com>
2011-10-12 11:44:59 -0400

    validaciones e idioma

--- app/models/vicariou.rb
+++ app/models/vicariou.rb
@@ -1,2 +1,10 @@
 class Vicariou < ActiveRecord::Base
+  validates_presence_of :nameVicariou
+  validates_presence_of :ubication
+  validates_presence_of :transport
+  validates_presence_of :telephone
+  validates_presence_of :vicarName
+  validates_numericality_of :telephone, :greater_than => 0, :if => "self.telephone.present?"
+  validates_uniqueness_of :nameVicariou, :message => "La Vicaria ya Existe"
+  validates_uniqueness_of :vicarName, :message => "El Vicario ya esta a cargo de una Vicaria"
 end
