c48d188acdbae22e7bd29ad6db587836a5fc2f8d
Valeria Tejada <vatesa09@gmail.com>
2011-10-12 08:22:01 -0400

    validacion de nombre en grupos

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,4 @@
 class Group < ActiveRecord::Base
+  #put validates here
+  validates_presence_of :name
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,12 +9,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111010123805) do
+ActiveRecord::Schema.define(:version => 20111011011312) do
 
   create_table "groups", :force => true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
 
   create_table "volunteers", :force => true do |t|
     t.string   "name"
