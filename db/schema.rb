f9b507df0bd10b237460e86cf942a84f4836fe02
alejandroMaldonado <alejandro.amg@hotmail.com>
2011-10-16 00:37:30 -0430

    arreglos

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111013141818) do
+ActiveRecord::Schema.define(:version => 20111015230855) do
 
   create_table "campaings", :force => true do |t|
     t.string   "name"
