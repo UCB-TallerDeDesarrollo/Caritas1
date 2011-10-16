5d931af637dd34cccb1b7e7a81709cf835da7179
LeonJorge <jorgerodrigoleonrgit@gmail.com>
2011-10-16 13:18:49 -0400

    Agregando funcionalidad al index

--- app/models/vicariou.rb
+++ app/models/vicariou.rb
@@ -7,4 +7,13 @@ class Vicariou < ActiveRecord::Base
   validates_numericality_of :telephone, :greater_than => 0, :if => "self.telephone.present?"
   validates_uniqueness_of :name_vicariou, :message => "La Vicaria ya Existe"
   validates_uniqueness_of :vicar_name, :message => "El Vicario ya esta a cargo de una Vicaria"
+  
+  def self.search(search)
+    if search
+      find(:all, :conditions => ['name_vicariou LIKE ? OR vicar_name LIKE ? OR ubication LIKE ? OR transport LIKE ?', "%#{search}%","%#{search}%","%#{search}%","%#{search}%"])
+    else
+      find(:all)
+    end
+  end
+
 end
