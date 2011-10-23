9beda0f0cdf7032697b1a4cc33744780e70f6f3f
Rolando Ismael <rolo555@hotmail.com>
2011-10-23 14:58:39 -0400

    index de social_works

--- app/models/social_work.rb
+++ app/models/social_work.rb
@@ -15,12 +15,19 @@ class SocialWork < ActiveRecord::Base
   #validates_numericality_of :phone, :greater_than => 1000000, :less_than => 9999999,:allow_blank => true  , :message => "El Celular no es un numero o es incorrecto"
   #validates_numericality_of :movil, :greater_than => 10000000, :less_than => 99999999,:allow_blank => true  ,:message => "El Celular no es un numero o es incorrecto"         
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{0,50})\Z/i, :allow_blank => true  
-  validates_format_of :social_work_name, :with => /^([a-zA-Z\ ]{3,50})$/i
-  validates_format_of :responsible, :with => /^([a-zA-Z\ ]{3,50})$/i
-  validates_format_of :responsible_filling, :with => /^([a-zA-Z\ ]{3,50})$/i
-  validates_format_of :location, :with => /^([a-zA-Z\ \-]{3,20})$/i
-  validates_format_of :parish, :with => /^([a-zA-Z\ ]{3,50})$/i
-  validates_format_of :social_work, :with => /^([a-zA-Z\ ]{3,50})$/i
+#  validates_format_of :social_work_name, :with => /^([a-zA-Z\ ]{3,50})$/i
+#  validates_format_of :responsible, :with => /^([a-zA-Z\ ]{3,50})$/i
+#  validates_format_of :responsible_filling, :with => /^([a-zA-Z\ ]{3,50})$/i
+#  validates_format_of :location, :with => /^([a-zA-Z\ \-]{3,20})$/i
+#  validates_format_of :parish, :with => /^([a-zA-Z\ ]{3,50})$/i
+#  validates_format_of :social_work, :with => /^([a-zA-Z\ ]{3,50})$/i
 
   #put class methods here
+  def self.search(search)
+    if search
+      find(:all, :conditions => ['social_work_name LIKE ?', "%#{search}%"])
+    else
+      find(:all)
+    end
+  end
 end
