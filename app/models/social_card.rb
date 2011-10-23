a721604cb05b36773e75c71544bfff0b8f963726
MarietaGonzales <Edgar@.(none)>
2011-10-22 23:56:35 -0400

    Modificacion y revalidacion de algunos campos

--- app/models/social_card.rb
+++ app/models/social_card.rb
@@ -3,22 +3,23 @@ class SocialCard < ActiveRecord::Base
   accepts_nested_attributes_for :helps
  
 
-  attr_accessible :date ,:parish_church,:name, :marital_status, :gender, :birthday, :birthday_location, :identity_card, :phone, :movil, :address, :location_reference, :grade_school, :occupation , :family_members,:parish_belongs, :family_history, :derivative_by,  :reference_person, :address_reference, :phone_reference, :movil_reference, :age_reference, :occupation_reference, :type_of_case, :did_you_get_another_help
+  attr_accessible :date ,:parish_church,:name, :marital_status, :gender, :birthday, :birthday_location, :identity_card, :phone, :movil, :address, :location_reference, :grade_school, :occupation , :family_members,:parish_belongs, :family_history, :derivative_by,  :reference_person, :address_reference, :phone_reference, :movil_reference, :age_reference, :occupation_reference, :type_of_case, :did_you_get_another_help, :what_kind
   attr_writer :current_step
 
   validates_presence_of :parish_church, :if => lambda { |o| o.current_step == "personal" }
   validates_uniqueness_of :name, :if => lambda { |o| o.current_step == "personal" }
-  validates_presence_of :name, :if => lambda { |o| o.current_step == "personal" }
+  validates_presence_of :name, :with => /^([a-zA-Z\ ]{3,50})$/i,:if => lambda { |o| o.current_step == "personal" }
   validates_presence_of :birthday, :if => lambda { |o| o.current_step == "personal" }
   validates_presence_of :birthday_location, :if => lambda { |o| o.current_step == "personal" }
   validates_presence_of :address, :if => lambda { |o| o.current_step == "personal" }
   validates_presence_of :identity_card, :if => lambda { |o| o.current_step == "personal" }
   validates_numericality_of :phone, :greater_than => 1000000, :less_than => 9999999,:allow_blank => true  , :message => "El Telefono no es un numero o es incorrecto" ,  :if => lambda { |o| o.current_step == "personal" }
   validates_numericality_of :movil, :greater_than => 10000000, :less_than => 99999999,:allow_blank => true  ,:message => "El Celular no es un numero o es incorrecto" ,  :if => lambda { |o| o.current_step == "personal" }
-  validates_presence_of :occupation, :if => lambda { |o| o.current_step == "personal" }
+  validates_presence_of :occupation, :with => /^([a-zA-Z\ ]{3,50})$/i, :if => lambda { |o| o.current_step == "personal" }
   validates_presence_of :family_members, :if => lambda { |o| o.current_step == "personal" }
 
-  validates_presence_of :reference_person, :if => lambda { |o| o.current_step == "reference" }
+  validates_presence_of :reference_person,:with => /^([a-zA-Z\ ]{3,50})$/i, :if => lambda { |o| o.current_step == "reference" }
+   validates_presence_of :address_reference,:with => /^([a-zA-Z\ ]{3,50})$/i, :if => lambda { |o| o.current_step == "reference" }
   validates_presence_of :phone_reference, :if => lambda { |o| o.current_step == "reference" }
   validates_numericality_of :phone_reference, :greater_than => 1000000, :less_than => 9999999,:allow_blank => true  , :message => "El Telefono no es un numero o es incorrecto" ,  :if => lambda { |o| o.current_step == "reference" }
   validates_numericality_of :movil_reference, :greater_than => 10000000, :less_than => 99999999,:allow_blank => true  ,:message => "El Celular no es un numero o es incorrecto" ,  :if => lambda { |o| o.current_step == "reference" }
