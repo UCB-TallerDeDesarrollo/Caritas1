513e276647f8de352b38afb12eda9c8f46508e68
alejandroMaldonado <alejandro.amg@hotmail.com>
2011-10-24 21:08:52 -0430

    migrations

--- db/migrate/20111025004413_insertar_campos_en_grupos.rb
+++ db/migrate/20111025004413_insertar_campos_en_grupos.rb
@@ -1,10 +1,10 @@
 class InsertarCamposEnGrupos < ActiveRecord::Migration
   def self.up  
     execute "insert into groups(id,name,volunteer_id) values(0,'',0);"
-    execute "insert into groups(id,name,volunteer_id) values(1,'Corazonsitos',0);"
-    execute "insert into groups(id,name,volunteer_id) values(2,'La piedad',0);"
-    execute "insert into groups(id,name,volunteer_id) values(3,'La piedra',0);"
-    execute "insert into groups(id,name,volunteer_id) values(4,'La razon',0);"
+    execute "insert into groups(name,volunteer_id) values('Corazonsitos',0);"
+    execute "insert into groups(name,volunteer_id) values('La piedad',0);"
+    execute "insert into groups(name,volunteer_id) values('La piedra',0);"
+    execute "insert into groups(name,volunteer_id) values('La razon',0);"
   end
 
   def self.down    
