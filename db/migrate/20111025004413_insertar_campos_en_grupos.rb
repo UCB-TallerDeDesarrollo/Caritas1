6f20dc0b5ab5d5fd1147a162b1f30e4b212ff12c
alejandroMaldonado <alejandro.amg@hotmail.com>
2011-10-24 21:20:55 -0430

    migrations

--- db/migrate/20111025004413_insertar_campos_en_grupos.rb
+++ db/migrate/20111025004413_insertar_campos_en_grupos.rb
@@ -5,6 +5,12 @@ class InsertarCamposEnGrupos < ActiveRecord::Migration
     execute "insert into groups(name,volunteer_id) values('La piedad',0);"
     execute "insert into groups(name,volunteer_id) values('La piedra',0);"
     execute "insert into groups(name,volunteer_id) values('La razon',0);"
+    execute "insert into volunteers(name,last_name,second_last_name,profession,phone_number,movil,date_of_birth,genre,position,group_id,email,state) values('alejandro','maldonado','gongora','doctor',2321332,232231,'2011-10-24','masculino','voluntario',1,'','A');"
+    execute "insert into volunteers(name,last_name,second_last_name,profession,phone_number,movil,date_of_birth,genre,position,group_id,email,state) values('maria','caceres','gongora','doctor',2321332,232231,'2011-10-24','masculino','voluntario',1,'','A');"
+    execute "insert into volunteers(name,last_name,second_last_name,profession,phone_number,movil,date_of_birth,genre,position,group_id,email,state) values('raul','Amurrio','gongora','doctor',2321332,232231,'2011-10-24','masculino','voluntario',1,'','A');"
+    execute "insert into volunteers(name,last_name,second_last_name,profession,phone_number,movil,date_of_birth,genre,position,group_id,email,state) values('mauricio','riguerea','leon','doctor',2321332,232231,'2011-10-24','masculino','voluntario',1,'','A');"
+    execute "insert into volunteers(name,last_name,second_last_name,profession,phone_number,movil,date_of_birth,genre,position,group_id,email,state) values('valeria','mejia','fernandez','doctor',2321332,232231,'2011-10-24','masculino','voluntario',1,'','A');"
+    
   end
 
   def self.down    
