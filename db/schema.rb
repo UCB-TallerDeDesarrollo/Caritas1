2d12ce9775000b67fda535c87e3aa00ccdc609af
valeriaMejia <valeria_andrea190789@hotmail.com>
2011-11-04 07:52:59 -0400

    aniadido columna de estado a vicarious

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111104013352) do
+ActiveRecord::Schema.define(:version => 20111104114450) do
 
   create_table "campaings", :force => true do |t|
     t.string   "name"
@@ -158,6 +158,7 @@ ActiveRecord::Schema.define(:version => 20111104013352) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "pastor_id"
+    t.integer  "state",         :default => 1
   end
 
   create_table "volunteers", :force => true do |t|
