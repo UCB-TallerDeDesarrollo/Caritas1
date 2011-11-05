be644edce3dfd93e64dc013360791052e7510630
valeriaMejia <valeria_andrea190789@hotmail.com>
2011-11-04 21:19:41 -0400

    commit

--- app/controllers/vicarious_controller.rb
+++ app/controllers/vicarious_controller.rb
@@ -14,12 +14,14 @@ class VicariousController < ApplicationController
   # GET /vicarious/1.xml
   def show
     @vicariou = Vicariou.find(params[:id])
-    @pastor = Pastor.find(@vicariou.pastor_id)
-    @parish = Parish.all(:select => "id,parish_name,ubication,telephone",:conditions=> ["id in (select id from parishes where vicariou_id= ?)","#{@vicariou.id}"])
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @vicariou }
-    end
+   
+      @pastor = Pastor.find(@vicariou.pastor_id)
+      @parish = Parish.all(:select => "id,parish_name,ubication,telephone",:conditions=> ["id in (select id from parishes where vicariou_id= ?)","#{@vicariou.id}"])
+      respond_to do |format|
+        format.html # show.html.erb
+        format.xml  { render :xml => @vicariou }
+      end
+  
   end
 
   # GET /vicarious/new
