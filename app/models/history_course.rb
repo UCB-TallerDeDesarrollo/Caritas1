d449097e8d6bf1c55cdf2685b89394e0fff97e02
Javier Pardo <javier.p.g1989@gmail.com>
2011-11-14 19:40:08 -0400

    guardo mis cambios para hacer merge con los nuevos cambios en el servidor

--- app/models/history_course.rb
+++ app/models/history_course.rb
@@ -1,13 +1,13 @@
 class HistoryCourse < ActiveRecord::Base
   belongs_to :course
   validates_presence_of :idGroup , :message => "El Grupo no puede ser vacio"
+  validates_format_of :idGroup, :with => /^([a-zA-Z ñÑ]{0,255})$/i , :message => "Solo acepta letras y espacios"
   validates_presence_of :idParish , :message => "La Parroquia no puede ser vacio"
+  validates_format_of :idParish, :with => /^([a-zA-Z ñÑ]{0,255})$/i , :message => "Solo acepta letras y espacios"
   validates_presence_of :idCourse , :message => "El Curso no puede ser vacio"
-  validates_format_of :idGroup,:with => /^([0-9]{0,3})$/i , :message => "El Campo de Grupo solo acepta numeros"
-  validates_format_of :idParish,:with => /^([0-9]{0,3})$/i , :message => "El Campo de Parroquia solo acepta numeros"
   validates_format_of :idCourse,:with => /^([0-9]{0,3})$/i , :message => "El Campo de Curso solo acepta numeros"
   validates_presence_of :instructor , :message => "El instructor no puede ser vacio"
-  validates_format_of :instructor, :with => /^([a-zA-Z ]{0,255})$/i , :message => "El Campo de instructor solo acepta letras y espacios."
+  validates_format_of :instructor, :with => /^([a-zA-Z ñÑ]{0,255})$/i , :message => "El Campo de instructor solo acepta letras y espacios."
 
   #put class methods here
   def self.search(search)
@@ -17,5 +17,7 @@ class HistoryCourse < ActiveRecord::Base
       find(:all)
     end
   end
+
+  
 
 end
