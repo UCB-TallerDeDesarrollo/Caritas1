62b9f7bd190409ade0a3cdacf9ef73af405ac7ba
Javier Pardo <javier.p.g1989@gmail.com>
2011-11-14 23:56:56 -0400

    sea rreglaron los mensajes de errores

--- app/models/history_course.rb
+++ app/models/history_course.rb
@@ -1,12 +1,12 @@
   class HistoryCourse < ActiveRecord::Base
   belongs_to :course
-  validates_presence_of :idGroup , :message => "El Grupo no puede ser vacio"
+  validates_presence_of :idGroup , :message => " no puede ser vacio"
   validates_format_of :idGroup, :with => /^([a-zA-Z ñÑ]{0,255})$/i , :message => " es demasiado grande o tiene caracteres invalidos"
-  validates_presence_of :idParish , :message => "La Parroquia no puede ser vacio"
+  validates_presence_of :idParish , :message => " no puede ser vacio"
   validates_format_of :idParish, :with => /^([a-zA-Z ñÑ]{0,255})$/i , :message => " es demasiado grande o tiene caracteres invalidos"
-  validates_presence_of :idCourse , :message => "El Curso no puede ser vacio"
-  validates_format_of :idCourse,:with => /^([0-9]{0,3})$/i , :message => "El Campo de Curso solo acepta numeros"
-  validates_presence_of :instructor , :message => "El instructor no puede ser vacio"
+  validates_presence_of :idCourse , :message => " no puede ser vacio"
+  validates_format_of :idCourse,:with => /^([0-9]{0,3})$/i , :message => " solo acepta numeros"
+  validates_presence_of :instructor , :message => " no puede ser vacio"
   validates_format_of :instructor, :with => /^([a-zA-Z ñÑ]{0,255})$/i , :message => "El Campo de instructor solo acepta letras y espacios."
 
   #put class methods here
