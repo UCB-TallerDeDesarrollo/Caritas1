3a8ce3f71aac67e33f1e601817428d4865b28608
alejandroMaldonado <alejandro.amg@hotmail.com>
2011-11-20 17:51:08 -0430

    cru de cursos - Valeria M

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,9 @@
 class Course < ActiveRecord::Base
+  validates_presence_of :name   
+  validates_presence_of :responsible
+  
+  validates_length_of :name, :within => 0..100  
+  validates_length_of :responsible, :within => 0..80
+  validates_length_of :description, :within => 0..200
+  validates_format_of :responsible, :with => /^([a-zA-Z\ \-]{3,100})$/i  
 end
