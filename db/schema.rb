f2b1b373e0f6000d1fb878a2900bca73fb9acf30
jhenierLopez <jhe_nier13@hotmail.com>
2011-11-25 19:35:27 -0400

    rebase commit

--- db/schema.rb
+++ db/schema.rb
@@ -9,8 +9,8 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111125155358) do
-  
+ActiveRecord::Schema.define(:version => 20111125223735) do
+
   create_table "assistance_lists", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
