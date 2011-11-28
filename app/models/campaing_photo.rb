3abe646e59624121ec31fbc18ab722c6dd01b6ac
jhenierLopez <jhe_nier13@hotmail.com>
2011-11-28 19:20:52 -0400

    validaciones

--- app/models/campaing_photo.rb
+++ app/models/campaing_photo.rb
@@ -1,4 +1,7 @@
 class CampaingPhoto < ActiveRecord::Base
   belongs_to :campaing
   has_attached_file :data
+  
+  validates_attachement_size :data, :less_than => 500.kilobytes
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
 end
