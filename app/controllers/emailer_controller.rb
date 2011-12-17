102339adc8891fef9146644b97d6ae22df6e3751
amapola <amapola-s@hotmail.com>
2011-12-17 09:20:05 -0500

    integracion de envio de mails

---
+++ app/controllers/emailer_controller.rb
@@ -0,0 +1,16 @@
+class EmailerController < ApplicationController
+ def sendmail
+    email = params["email"]
+	  recipient = email["recipient"]
+	  subject = email["subject"]
+	  message = email["message"]
+      Emailer.deliver_contact(recipient, subject, message)
+      return if request.xhr?
+      contact
+ end
+ def contact
+   render :text => 'Mensaje Enviado Con Exito!'
+ end
+  def index
+   end
+end
---
+++ app/models/emailer.rb
@@ -0,0 +1,12 @@
+class Emailer < ActionMailer::Base
+  def contact(recipient, subject, message, sent_at = Time.now)
+      @subject = subject
+      @recipients = recipient
+      @from = 'no-reply@caritascbba.org'
+      @sent_on = sent_at
+        @body["title"] = subject
+        @body["email"] = 'no-reply@caritascbba.org'
+        @body["message"] = message
+      @headers = {}
+   end
+end
