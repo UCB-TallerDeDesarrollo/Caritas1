5bd8e98e1a42bb1c05fad61463adb4b3f983be81
amapola <amapola-s@hotmail.com>
2011-12-17 15:14:37 -0500

    pagina de contacto

--- app/models/emailer.rb
+++ app/models/emailer.rb
@@ -8,5 +8,15 @@ class Emailer < ActionMailer::Base
         @body["email"] = 'no-reply@caritascbba.org'
         @body["message"] = message
       @headers = {}
+  end
+  def contact_us(recipient, subject, message, sent_at = Time.now)
+      @subject = subject
+      @recipients = recipient
+      @from = 'no-reply@caritascbba.org'
+      @sent_on = sent_at
+        @body["title"] = subject
+        @body["recipient"] = recipient
+        @body["message"] = message
+      @headers = {}
    end
 end
