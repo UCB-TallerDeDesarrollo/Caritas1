9782e531a30b7db8521ff0e1e57b1841022f42ff
Oscar Cardenas <che1406@gmail.com>
2011-12-19 15:54:01 -0400

    Eliminados datos Antiguos de Pastores y Vicarias

--- db/migrate/20111025015034_ingresar_datos_pastores.rb
+++ db/migrate/20111025015034_ingresar_datos_pastores.rb
@@ -1,13 +1,7 @@
 class IngresarDatosPastores < ActiveRecord::Migration
-def self.up  
-    execute "insert into pastors(name,primary_last_name,second_last_name,phone_number,cellphone_number) values('Juan','Souza','Divaglio',10,20);"
-    execute "insert into pastors(name,primary_last_name,second_last_name,phone_number,cellphone_number) values('Pedro','Mustafa','Loktar',120,34);"
-    execute "insert into pastors(name,primary_last_name,second_last_name,phone_number,cellphone_number) values('Lucas','Dimatelo','Dimitri',103,25);"
-    execute "insert into pastors(name,primary_last_name,second_last_name,phone_number,cellphone_number) values('Pablo','Chumacero','Bacalao',30,21);"
-    execute "insert into pastors(name,primary_last_name,second_last_name,phone_number,cellphone_number) values('Bartolomeo','Cuevas','Villa',130,43);"
+def self.up     
   end
 
-  def self.down    
-    execute "truncate table pastors"   
+  def self.down        
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111219134203) do
+ActiveRecord::Schema.define(:version => 20111219193912) do
 
   create_table "assistance_lists", :force => true do |t|
     t.datetime "created_at"
