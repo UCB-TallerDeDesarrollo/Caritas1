1074882fc880fb79d6b2fdd72ec5cc77d1c3c8d5
Oscar Cardenas <che1406@gmail.com>
2011-12-19 16:14:11 -0400

    Eliminados los datos referentes a Voluntarios, Grupos, Vicarias,etc

--- db/migrate/20111025015034_ingresar_datos_pastores.rb
+++
@@ -1,7 +0,0 @@
-class IngresarDatosPastores < ActiveRecord::Migration
-def self.up     
-  end
-
-  def self.down        
-  end
-end
--- db/migrate/20111219193912_eliminando_los_grupos_existentes.rb
+++
@@ -1,8 +0,0 @@
-class EliminandoLosGruposExistentes < ActiveRecord::Migration
-  def self.up
-  end
-
-  def self.down
-        execute "truncate table groups"   
-  end
-end
