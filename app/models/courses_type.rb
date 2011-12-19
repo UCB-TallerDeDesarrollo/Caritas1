729a65eb2e60b5b70b25fcf083fdcacc36025504
Daniel Rocabado <danrocar@gmail.com>
2011-12-18 20:43:22 -0400

    Validacion de formato y tamaho de campos en Cursos de Formacion@

--- app/models/courses_type.rb
+++ app/models/courses_type.rb
@@ -2,11 +2,13 @@ class CoursesType < ActiveRecord::Base
   file_column :course_file
   validates_filesize_of :course_file, :in => 1.kilobytes..10000.kilobytes, :message => "no puede ser m'as grande de 10 MB"
 
+  
   validates_presence_of :name
   validates_presence_of :description 
   
   validates_length_of :name, :within => 0..150
   #validates_length_of :description, :within => 0..255
+  
   
   
   #has_many :courses
