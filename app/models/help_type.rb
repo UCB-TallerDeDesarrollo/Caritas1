d83a218f836cf72a7bc598b55e788f3382b03b2f
Pablo Sanabria <pablo.s.q1.4.1991@gmail.com>
2012-10-15 19:51:56 -0400

    Arreglado un bug en el model help_type

--- app/models/help_type.rb
+++ app/models/help_type.rb
@@ -2,5 +2,5 @@ class HelpType < ActiveRecord::Base
   validates_presence_of :name
   validates_presence_of :description
   
-  validates_lenght_of :name, :maximum => 50  
+  validates_length_of :name, :maximum => 50  
 end
