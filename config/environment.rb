5b537166a577db0f80f540d95797ec0a21e51080
Pablo Sanabria <pablo.s.q1.4.1991@gmail.com>
2012-10-28 22:48:11 -0400

    Testing roles

--- config/environment.rb
+++ config/environment.rb
@@ -42,6 +42,7 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
+  config.gem "declarative_authorization"
 end
 ActionMailer::Base.smtp_settings = {
    :enable_starttls_auto => true,
