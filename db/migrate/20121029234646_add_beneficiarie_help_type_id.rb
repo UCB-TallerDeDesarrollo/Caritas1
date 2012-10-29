ce9b93414ef3c93fc32474ee070485fadb3c8c3e
Ariel <ariel_bc91@hotmail.com>
2012-10-29 19:51:05 -0400

    arreglo en tabla beneficiary

---
+++ db/migrate/20121029234646_add_beneficiarie_help_type_id.rb
@@ -0,0 +1,13 @@
+class AddBeneficiarieHelpTypeId < ActiveRecord::Migration
+  
+  def self.up
+
+	add_column :beneficiaries, :help_type_id, :integer
+  end
+
+
+  def self.down
+	remove_column :beneficiaries, :help_type_id
+  end
+
+end
