08c6692969af2aeb5032fd3e1f7b4d42c53d4116
pablojoa <jim_24991@hotmail.com>
2012-10-28 20:12:10 -0400

    Reporte de ayudas filtrando por fechas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 ActionController::Routing::Routes.draw do |map|
-  map.resources :beneficiary_helps
+  map.resources :beneficiary_helps, :collection => {:report=>:get}
 
   map.resources :help_types
 
