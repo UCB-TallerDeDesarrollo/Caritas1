09764539edc7a65003eb4e8b741f20c9ba43a79a
unknown <Victor@.(none)>
2012-11-25 11:39:15 -0430

    Reporte de ayudas recibidas permitir ordenar  por parroquia y por apellido

--- app/models/beneficiary_help.rb
+++ app/models/beneficiary_help.rb
@@ -7,23 +7,23 @@ class BeneficiaryHelp < ActiveRecord::Base
   #put class methods here
   def self.search(date_start, date_end, help_type, parish_id, beneficiary_type_id)
     if help_type && date_start && date_end && parish_id && beneficiary_type_id
-      find(:all, :conditions => [ ' date between ? and ? and beneficiaries.beneficiary_type_id = ? and "helpType" = ? and beneficiaries.parish_id = ?', date_start,date_end,beneficiary_type_id,help_type,parish_id ], :include=>[:beneficiary=>[:parish]],:order => 'parishes.parish_name,date')
+      find(:all, :conditions => [ ' date between ? and ? and beneficiaries.beneficiary_type_id = ? and "helpType" = ? and beneficiaries.parish_id = ?', date_start,date_end,beneficiary_type_id,help_type,parish_id ], :include=>[:beneficiary=>[:parish]],:order => 'parishes.parish_name,beneficiaries.last_name')
     elsif date_start && date_end && help_type && beneficiary_type_id
-      find(:all, :conditions => [ ' date between ? and ? beneficiaries.beneficiary_type_id = ? and "helpType" = ?', date_start,date_end,beneficiary_type_id,help_type], :include=>[:beneficiary=>[:parish]],:order => 'parishes.parish_name,date')
+      find(:all, :conditions => [ ' date between ? and ? beneficiaries.beneficiary_type_id = ? and "helpType" = ?', date_start,date_end,beneficiary_type_id,help_type], :include=>[:beneficiary=>[:parish]],:order => 'parishes.parish_name,beneficiaries.last_name')
     elsif date_start && date_end && parish_id && beneficiary_type_id
-      find(:all, :conditions => [ ' date between ? and ? and beneficiaries.beneficiary_type_id = ? and beneficiaries.parish_id = ?', date_start,date_end,beneficiary_type_id,parish_id ], :include=>[:beneficiary=>[:parish]],:order => 'parishes.parish_name,date')
+      find(:all, :conditions => [ ' date between ? and ? and beneficiaries.beneficiary_type_id = ? and beneficiaries.parish_id = ?', date_start,date_end,beneficiary_type_id,parish_id ], :include=>[:beneficiary=>[:parish]],:order => 'parishes.parish_name,beneficiaries.last_name')
     elsif date_start && date_end && beneficiary_type_id
-      find(:all, :conditions => [ ' date between ? and ? and beneficiaries.beneficiary_type_id = ? ', date_start,date_end,beneficiary_type_id], :include=>[:beneficiary=>[:parish]],:order => 'parishes.parish_name,date')
+      find(:all, :conditions => [ ' date between ? and ? and beneficiaries.beneficiary_type_id = ? ', date_start,date_end,beneficiary_type_id], :include=>[:beneficiary=>[:parish]],:order => 'parishes.parish_name,beneficiaries.last_name')
     elsif help_type && date_start && date_end && parish_id
-      find(:all, :conditions => [ ' date between ? and ? and "helpType" = ? and beneficiaries.parish_id = ?', date_start,date_end,help_type,parish_id ], :include=>[:beneficiary=>[:parish]],:order => 'parishes.parish_name,date')
+      find(:all, :conditions => [ ' date between ? and ? and "helpType" = ? and beneficiaries.parish_id = ?', date_start,date_end,help_type,parish_id ], :include=>[:beneficiary=>[:parish]],:order => 'parishes.parish_name,beneficiaries.last_name')
     elsif date_start && date_end && help_type
-      find(:all, :conditions => [ ' date between ? and ? and "helpType" = ?', date_start,date_end,help_type], :include=>[:beneficiary=>[:parish]],:order => 'parishes.parish_name,date')
+      find(:all, :conditions => [ ' date between ? and ? and "helpType" = ?', date_start,date_end,help_type], :include=>[:beneficiary=>[:parish]],:order => 'parishes.parish_name,beneficiaries.last_name')
     elsif date_start && date_end && parish_id
-      find(:all, :conditions => [ ' date between ? and ? and beneficiaries.parish_id = ?', date_start,date_end,parish_id ], :include=>[:beneficiary=>[:parish]],:order => 'parishes.parish_name,date')
+      find(:all, :conditions => [ ' date between ? and ? and beneficiaries.parish_id = ?', date_start,date_end,parish_id ], :include=>[:beneficiary=>[:parish]],:order => 'parishes.parish_name,beneficiaries.last_name')
     elsif date_start && date_end
-      find(:all, :conditions => [ ' date between ? and ? ', date_start,date_end],:include=>[:beneficiary=>[:parish]],:order => 'parishes.parish_name,date')
+      find(:all, :conditions => [ ' date between ? and ? ', date_start,date_end],:include=>[:beneficiary=>[:parish]],:order => 'parishes.parish_name,beneficiaries.last_name')
     else
-      find(:all, :include=>[:beneficiary=>[:parish]],:order => 'parishes.parish_name,date')
+      find(:all, :include=>[:beneficiary=>[:parish]],:order => 'parishes.parish_name,beneficiaries.last_name')
     end
   end
 
@@ -31,7 +31,7 @@ class BeneficiaryHelp < ActiveRecord::Base
     if date_start && date_end
       #    find(:all, :joins, "" :conditions => [ " date between ? and ? AND parish_id = ?", date_start,date_end,parish_help ], :order => "date")
       else
-      find(:all, :order => "date")
+      find(:all, :order => 'parishes.parish_name,beneficiaries.last_name')
     end
   end
 
