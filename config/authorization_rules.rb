b42258f0b3383862261e59458adb0f320d1aaf99
unknown <Victor@.(none)>
2012-11-28 15:50:43 -0430

    el rol voluntario puede ver la informacio  de todos los grupos

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -51,10 +51,8 @@ authorization do
   role :voluntario do
     includes :guest
     has_permission_on [:volunteers], :to => [:show, :edit, :update]
-    has_permission_on [:groups], :to => [:index]
-    has_permission_on :groups, :to => :show do
-      if_attribute :volunteers => contains { Volunteer.find(user.controller_type) }
-    end
+    has_permission_on [:groups], :to => [:index,:show]
+
     has_permission_on :volunteers, :to => :index_show do
       if_attribute :volunteers => contains { Volunteer.find(user.controller_type) }
     end
